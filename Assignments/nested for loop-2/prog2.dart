bdd91ae65838c8aeee5b929f06b6b7528c92ba45
Deepta69 <130881774+Deepta69@users.noreply.github.com>
2024-09-20 21:16:20 +0530

    patterns 2

---
+++ Assignments/nested for loop-2/prog2.dart
@@ -0,0 +1,15 @@
+
+ import 'dart:io';
+void main(){
+  int row=4;
+ int n=row;
+  for(int i=1; i<=row; i++){
+     
+    for(int j=1; j<=i; j++){
+      stdout.write("$n ");
+      
+    }
+    print('');
+    n--;
+  }
+}
